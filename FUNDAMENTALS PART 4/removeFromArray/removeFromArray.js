6290d96c5c299fcee957d6475d30246ffd92fd9b
Ali Alohali <ali.alohali@gmail.com>
2021-01-15 20:41:54 +0300

    done with all exercises

--- FUNDAMENTALS PART 4/removeFromArray/removeFromArray.js
+++ FUNDAMENTALS PART 4/removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let toRemove = args.slice(1);
+      let finalResult = [];
+      for(i=0;i<args[0].length;i++){
+        if(!toRemove.includes(args[0][i])){
+          finalResult.push(args[0][i]);
+        }
+      }
+      return finalResult;
 }
 
 module.exports = removeFromArray
--- FUNDAMENTALS PART 4/reverseString/reverseString.js
+++ FUNDAMENTALS PART 4/reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let newText = [];
+    for(i=text.length;i>=0;i--){
+      newText.push(text[i]);
+    }
+    return newText.join("");
 }
 
 module.exports = reverseString
